4f698f1894f8d80a75510cd252b3c2be4ae35d36
roman9891 <roman9891@gmail.com>
2020-12-20 15:52:37 -0500

    added comments to bubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,14 +1,13 @@
 const bubbleSort = arr => {
-    let iterations = 0
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - 1 - i; j++) {
+            // - 1 so last index doesn't compare vs undefined
+            // - i so that loop shortens as the items at the end get sorted
             if (arr[j] > arr[j+1]) {
-                [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]]//es2015 syntax for swapping
             }
-            iterations++
         }
     }
-    console.log(iterations)
     return arr
 }
 
