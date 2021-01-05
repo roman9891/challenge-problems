58dcf7ff48feab29d0366488d77db4e743b8e1ec
roman9891 <roman9891@gmail.com>
2021-01-04 23:08:44 -0500

    added quadratic example

---
+++ quadraticTime.js
@@ -0,0 +1,9 @@
+const quadratic = (n) => {
+    for (let i = 0; i < n; i++){
+        console.log("I'll print n times")
+        for (let j = 0; j < n; j++) {
+            console.log("I'll print n*n times (n^2)")
+        }
+    }
+}
+
