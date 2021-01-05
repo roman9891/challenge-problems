891a11b829b30bebbe1067982df799d2c70d1a37
roman9891 <roman9891@gmail.com>
2021-01-04 20:03:38 -0500

    added linear examples

---
+++ bigOLinear.js
@@ -0,0 +1,13 @@
+const linear = (n) => {
+    let i = 0
+    while (i < n) {
+        console.log("Doing a thing!")
+        i++
+    }
+}
+
+const linear = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        console.log("Doing a thing!")
+    }
+}
