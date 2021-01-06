8fb4b6a49805750140203625c7fcbe34197bfe0a
roman9891 <roman9891@gmail.com>
2021-01-06 08:45:46 -0500

    link list pop method

--- singleLinkedList.js
+++ singleLinkedList.js
@@ -16,6 +16,42 @@ class SinglylinkedList{
         if (!this.head) {
             this.head = newNode
             this.tail = this.head
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
         }
+        this.length++
+        return this
+    }
+    pop(){
+        if(!this.head) return undefined//if list is empty return undefined
+
+        let current = this.head, 
+            pre = current
+
+        while (current.next) {//traverse list until there is no next and keep track of previous node
+            pre = current
+            current = current.next
+        }
+
+        this.tail = pre//previous becomes new tail
+        this.tail.next = null//this severs connection to previous tail
+        this.length--
+
+        if (this.length === 0) {//reset list if last item popped
+            this.head = null
+            this.tail = null
+        }
+
+        return current
     }
 }
+
+const list = new SinglylinkedList()
+list.push(1)
+list.push(2)
+list.push(3)
+console.log(list)
+list.pop()
+console.log(list.pop(), list)
+console.log(list.pop(), list)
