a81a1f012773f7f14ef2cd8fe5292337ebadabc5
roman9891 <roman9891@gmail.com>
2021-05-14 07:30:13 -0400

    added arrayStringsAreEqual solution

---
+++ arrayStringsAreEqual.js
@@ -0,0 +1,4 @@
+// given two arrays containing groups of characters
+// return whether the arrays make equal strings when their respective groups are joined
+
+const arrayStringsAreEqual = (word1, word2) => word1.join('') === word2.join('')
