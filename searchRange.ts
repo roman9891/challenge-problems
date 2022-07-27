56c8028e5b26a832e0548c8e722fc1ef2e9465ce
Roman Opalacz <roman9891@gmail.com>
2022-07-27 09:59:19 -0400

    passing solution

--- searchRange.ts
+++ searchRange.ts
@@ -35,8 +35,8 @@ const searchRange = (nums: number[], target: number): number[] => {
     // once target is found look left until not target and vice versa
     // push left and right indices of target into result and return
     const index = binSearch(nums, target)
-    
-    if (index) {
+
+    if (index !== null) {
         return findRange(nums, index)
     } else {
         return [-1,-1]
@@ -44,16 +44,41 @@ const searchRange = (nums: number[], target: number): number[] => {
 }
 
 const binSearch = (nums: number[], target: number): number | null => {
-    let start, mid, end
+    let start = 0
+    let end = nums.length - 1
+    let mid = Math.floor((start + end) / 2)
+
+    while (nums[mid] !== target && start <= end) {
+        if (target < nums[mid]) end = mid - 1
+        else start = mid + 1
+        mid = Math.floor((start + end) / 2)
+    }
 
     return nums[mid] === target ? mid : null
 }
 
 const findRange = (nums: number[], index: number): number[] => {
-    const result = []
+    const target = nums[index]
+    const result: number[] = []
+    let leftIndex = index
+    let rightIndex = index
+
+    while (nums[leftIndex - 1] === target) {
+        leftIndex = leftIndex - 1
+    }
+
+    result.push(leftIndex)
+
+    while (nums[rightIndex + 1] === target) {
+        rightIndex = rightIndex + 1
+    }
+
+    result.push(rightIndex)
+
     return result
 }
 
 console.log(searchRange([5,7,7,8,8,10], 8)) // [3,4]
 console.log(searchRange([5,7,7,8,8,10], 6)) // [-1,-1]
 console.log(searchRange([], 0)) // [-1,-1]
+console.log(binSearch([2,2], 2))
