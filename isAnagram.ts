13afc1cab962d210ea026558080ae6b40b426a46
Roman Opalacz <roman9891@gmail.com>
2022-07-28 09:22:28 -0400

    planning

---
+++ isAnagram.ts
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/valid-anagram/
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+// typically using all the original letters exactly once.
+
+// Example 1:
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+
+// Example 2:
+// Input: s = "rat", t = "car"
+// Output: false
+ 
+// Constraints:
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letters.
+
+const isAnagram = (s: string, t: string): boolean => {
+    // create tracking object for each letter in s
+    // assign each key as a letter
+    // assign each value that letters count
+    // iterate through t
+    // subtract count from tracker
+    // loop through tracker
+    // if any value isn't zero return false
+    // otherwise true
+    return true
+}
+
+console.log(isAnagram("anagram", "nagaram")) // true
+console.log(isAnagram("rat", "car")) // false
