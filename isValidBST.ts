c3d85e4e0a6fb84734db31594b48a903d96da9fa
Roman Opalacz <roman9891@gmail.com>
2022-08-12 11:41:39 -0400

    passing solution

--- isValidBST.ts
+++ isValidBST.ts
@@ -30,7 +30,7 @@ class TreeNode {
     }
 }
 
-const isValidBST = (root: TreeNode | null): boolean => {
+// const isValidBST = (root: TreeNode | null): boolean => {
     // BFS traversal of tree
     // save visited list
     // filter list of null values
@@ -38,15 +38,80 @@ const isValidBST = (root: TreeNode | null): boolean => {
     // BFS traversal of new tree
     // compare new list to unfiltered original list
 
-    const originalTreeArray = breadthFirstSearch(root)
-    const filteredOriginalTree = originalTreeArray.filter(value => value)
-    const newTreeRoot = new TreeNode(originalTreeArray[0])
+//     const originalTreeArray = breadthFirstSearch(root)
+//     const filteredOriginalTree = originalTreeArray.filter(value => value)
+//     const newTreeRoot = new TreeNode(originalTreeArray[0])
 
-    for (let i = 1; i < originalTreeArray.length; i++) {}
+//     for (let i = 1; i < originalTreeArray.length; i++) {}
+
+//     const newTreeArray = breadthFirstSearch(newTreeRoot)
+
+//     return originalTreeArray.join('') === newTreeArray.join('')
+// }
+
+const isValidBST = (root: TreeNode | null): boolean => {
+    if (!root) return true
+    if (!root.left && !root.right) return true
+    const originalTreeArray = breadthFirstSearch(root)
+    const filteredOriginalTree = originalTreeArray.filter(value => value !== null)
+    const newTreeRoot = filteredOriginalTree[0] ? new TreeNode(filteredOriginalTree[0]) : null
+
+    for (let i = 1; i < filteredOriginalTree.length; i++) {
+        let currentValue = filteredOriginalTree[i]
+        if (currentValue !== null) insert(newTreeRoot, currentValue)
+    }
 
     const newTreeArray = breadthFirstSearch(newTreeRoot)
+    
+    return originalTreeArray.map(value => value === null ? "null" : value).join('') === newTreeArray.map(value => value === null ? "null" : value).join('')
+};
 
-    return originalTreeArray.join('') === newTreeArray.join('')
+const breadthFirstSearch = (root: TreeNode | null): (number | null)[] => {
+    if (!root) return []
+
+    let current: TreeNode | undefined | null = root
+    const queue: [TreeNode|null] = [current]
+    const visited: (number | null)[] = []
+
+    while (queue.length) {
+        current = queue.shift()
+        if (current) {
+            visited.push(current.val)
+            if (current.left) queue.push(current.left)
+            else queue.push(null)
+            if (current.right) queue.push(current.right)
+            else queue.push(null)
+        }
+        else visited.push(null)
+    }
+
+    return visited
+}
+
+const insert = (root: TreeNode | null, value: number): void => {
+    if (root === null) return
+    let newNode = new TreeNode(value)
+
+    // if (!this.root) this.root = newNode
+        // let root = this.root
+    let searching = true
+
+    while (searching) {
+        if (newNode.val === root.val) return
+        if (value > root.val) {
+            if (root.right) root = root.right
+            else {
+                root.right = newNode
+                searching = false
+            }
+        } else if (value < root.val) {
+            if (root.left) root = root.left
+            else {
+                root.left = newNode
+                searching = false
+            }
+        }
+    }
 }
 
 //console.log(isValidBST([2,1,3])) // true
