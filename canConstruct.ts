0c3d19419239aebbc90dc321b54953de71c787d2
Roman Opalacz <roman9891@gmail.com>
2022-08-25 08:43:48 -0400

    setup

---
+++ canConstruct.ts
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/ransom-note/
+
+// Given two strings ransomNote and magazine, 
+// return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+// Each letter in magazine can only be used once in ransomNote.
+
+// Example 1:
+// Input: ransomNote = "a", magazine = "b"
+// Output: false
+
+// Example 2:
+// Input: ransomNote = "aa", magazine = "ab"
+// Output: false
+
+// Example 3:
+// Input: ransomNote = "aa", magazine = "aab"
+// Output: true
+
+// Constraints:
+// 1 <= ransomNote.length, magazine.length <= 105
+// ransomNote and magazine consist of lowercase English letters.
+
+const canConstruct = (ransomNote: string, magazine: string): boolean => {
+    return true
+}
+
+console.log(canConstruct("a","b")) // false
+console.log(canConstruct("aa", "ab")) // false
+console.log(canConstruct("aa", "aab")) // true
