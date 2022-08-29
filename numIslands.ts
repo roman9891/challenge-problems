4b5837e4c727f1fcd0126451be93b38e116a1278
Roman Opalacz <roman9891@gmail.com>
2022-08-29 17:13:00 -0400

    time exceeding solution

--- numIslands.ts
+++ numIslands.ts
@@ -36,22 +36,102 @@ const numIslands = (grid: string[][]): number => {
     // iterate through grid
     // if 1 check adjacency
     //   iterate through found islands and see if any adjacent tiles have already been found
-    //   if so then add tile to that island
+    //   if so then track that island
+    //   after checking all sides
+    //   if only adjacent to one island then add to that island
+    //   if adjacent to multiple islands then merge islands and add to new island
     //   if not then add as new island
     // return island count
     const foundIslands: Island[] = []
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            const currentTile = grid[i][j]
+
+            if (currentTile === "1") checkAdjacency(i, j, grid, foundIslands)
+        }
+    } 
     return foundIslands.length
 }
 
+const checkAdjacency = (i: number, j: number, grid: string[][], foundIslands: Island[]): void => {
+    const coordinates = `${i}x${j}`
+    const adjacentIslands: Island[] = []
+    
+    if (grid[i-1] && grid[i-1][j]) {
+        const coordinate = `${i-1}x${j}`
+        const island = foundIslands.find(island => island[coordinate])
+        if (island && !adjacentIslands.includes(island)) adjacentIslands.push(island)
+    }
+    if (grid[i][j+1]) {
+        const coordinate = `${i}x${j+1}`
+        const island = foundIslands.find(island => island[coordinate])
+        if (island && !adjacentIslands.includes(island)) adjacentIslands.push(island)
+    }
+    if (grid[i+1] && grid[i+1][j]) {
+        const coordinate = `${i+1}x${j}`
+        const island = foundIslands.find(island => island[coordinate])
+        if (island && !adjacentIslands.includes(island)) adjacentIslands.push(island)
+    }
+    if (grid[i][j-1]) {
+        const coordinate = `${i}x${j-1}`
+        const island = foundIslands.find(island => island[coordinate])
+        if (island && !adjacentIslands.includes(island)) adjacentIslands.push(island)
+    }
+
+    if (adjacentIslands.length > 1) {
+        // merge adjacent islands
+        // add coordinates to new island
+        const island = mergeIslands(adjacentIslands, foundIslands)
+        island[coordinates] = true
+    } else if (adjacentIslands.length === 1) {
+        // add coordinates to island
+        const island = adjacentIslands[0]
+        island[coordinates] = true
+    } else {
+        const newIsland: Island = {}
+        newIsland[coordinates] = true
+        foundIslands.push(newIsland)
+    }
+}
+
+const mergeIslands = (adjacentIslands: Island[], foundIslands: Island[]): Island => {
+    const newIsland: Island = {}
+    const coordinates: string[] = []
+
+    adjacentIslands.forEach(island => {
+        Object.keys(island).forEach(key => coordinates.push(key))
+    })
+
+    coordinates.forEach(coordinate => newIsland[coordinate] = true)
+
+    // deleteIsland
+    adjacentIslands.forEach(island => {
+        const index = foundIslands.findIndex(foundIsland => foundIsland === island)
+        foundIslands.splice(index, 1)
+    })
+
+    foundIslands.push(newIsland)
+
+    return newIsland
+}
+
+// console.log(numIslands([
+//        ["1","1","1","1","0"],
+//        ["1","1","0","1","0"],
+//        ["1","1","0","0","0"],
+//        ["0","0","0","0","0"]
+//      ]))
+// console.log(numIslands([
+//        ["1","1","0","0","0"],
+//        ["1","1","0","0","0"],
+//        ["0","0","1","0","0"],
+//        ["0","0","0","1","1"]
+//      ]))
 console.log(numIslands([
-       ["1","1","1","1","0"],
-       ["1","1","0","1","0"],
-       ["1","1","0","0","0"],
-       ["0","0","0","0","0"]
-     ]))
-console.log(numIslands([
-       ["1","1","0","0","0"],
-       ["1","1","0","0","0"],
-       ["0","0","1","0","0"],
-       ["0","0","0","1","1"]
-     ]))
+    ["1","1","1","1","1","1"],
+    ["1","0","0","0","0","1"],
+    ["1","0","1","1","0","1"],
+    ["1","0","0","0","0","1"],
+    ["1","1","1","1","1","1"]
+]))
